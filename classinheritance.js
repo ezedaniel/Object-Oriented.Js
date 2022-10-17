d430c18b5e70744a6c73df1814701f2cf8c88a25
Daniel Eze <eze_d@yahoo.com>
2022-10-17 17:04:59 +0100

    Update classinheritance.js

--- classinheritance.js
+++ classinheritance.js
@@ -1,7 +1,35 @@
+/* as per user created on the object literal script herein */
+
+class User {
+    constructor(email, firstName,lastName, age){
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, 'Welcome');
+        return this
+    }
+    logout(){
+        console.log(this.email, 'Bye');
+        return this
+    }
+    updatescore(){
+        this.score ++;
+        console.log (this.email, 'your score is', this.score);
+        return this
+    }
+};
+
+/* To create an admin that would have some special features that other users cant have, rather than making a new class for the
+admin and adding all same methods of a user but including the specail ability.
+
+its best to create a class that inherits the user method
 
 
-
-
+*/
 
 class Admin extends User {
     deleteUser(user){
@@ -11,9 +39,9 @@ class Admin extends User {
     }
 }
 
-var userOne = new User('ryu@ninjas.com', 'Ryu');
-var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-var admin = new Admin('shaun@ninjas.com', 'Shaun');
+var userOne = new User('daniel_eze@gmail.com', 'Daniel', 'Eze', '29');
+var userTwo = new User('ofoedusimon@gmail.com', 'Simon', 'Ofoedu', '30');
+var admin = new Admin('tucker@yahoo.com', 'Tucker', 'Daniel', '33');
 
 var users = [userOne, userTwo, admin];
 
